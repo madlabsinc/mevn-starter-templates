75a8dffacb32b1390b4b164beca6238d829b9871
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-06-28 17:02:07 +0530

    Fix linting errors

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,7 @@
-
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import VueApollo from 'vue-apollo'
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import VueApollo from "vue-apollo";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
@@ -11,21 +10,21 @@ Vue.config.productionTip = false;
 
 const httpLink = new HttpLink({
   // URL to graphql server, you should use an absolute URL here
-  uri: 'http://localhost:5000/graphql'
-})
+  uri: "http://localhost:5000/graphql"
+});
 
 // create the apollo client
 const apolloClient = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
-})
+});
 
 const apolloProvider = new VueApollo({
   defaultClient: apolloClient
-})
+});
 
 // install the vue plugin
-Vue.use(VueApollo)
+Vue.use(VueApollo);
 
 new Vue({
   router,
