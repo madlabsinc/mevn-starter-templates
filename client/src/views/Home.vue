5c787d47db008152c466cb9be0b1b03146f646e1
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-08-25 21:29:42 +0530

    customize template

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Vue logo" src="../assets/mevn.jpg" />
+    <HelloWorld msg="PWA Boilerplate Template" />
   </div>
 </template>
 
